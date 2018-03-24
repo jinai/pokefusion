320ac7b41d6ef0a878b41b50c9f298616dd2e752
Jinai <7669687+Jinai@users.noreply.github.com>
2018-03-24 01:22:28 +0100

    Remove unnecessary cast to list, refactor

--- pokefusion/pokedex.py
+++ pokefusion/pokedex.py
@@ -17,7 +17,7 @@ class Language(Enum):
 class Pokedex:
     DATA_DIR = "data"
     FILE_PATTERN = "pokedex_*.json"
-    RANDOM_QUERY = {"random", "rand", "r", "?", "x", "."}
+    RANDOM_QUERY = {"random", "rand", "rnd", "r", "?", "x", "."}
 
     def __init__(self):
         self.data = {}
@@ -25,17 +25,22 @@ class Pokedex:
             lang = Language(splitext(basename(path))[0].split("_")[-1])  # pokedex_{lang}.json
             with open(path, "r", encoding="utf-8") as f:
                 raw = json.load(f)
-                normalized = {key: utils.normalize(value) for key, value in list(raw.items())}
+                normalized = {key: utils.normalize(value) for key, value in raw.items()}
                 self.data[lang] = utils.TwoWayDict(normalized)
 
     def resolve(self, query, lang):
         """Returns (id, name)"""
+
+        # Example : !fusion 122
         if query.isdigit():
             return query, self.data[lang][query]
 
+        # Example : !fusion ? bulbasaur
         if query in Pokedex.RANDOM_QUERY:
-            r = str(random.randint(0, 151))
-            return r, self.data[lang][r]
+            rand = str(random.randint(0, 151))
+            return rand, self.data[lang][rand]
+
+        # Example : !fusion mr.mime
         else:
-            n = utils.normalize(query)
-            return self.data[lang][n], n
+            query = utils.normalize(query)
+            return self.data[lang][query], query
