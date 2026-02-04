f4d80144a95fbd8e6d26a88fb3daaeaa2c1da38b
jinai <7669687+jinai@users.noreply.github.com>
2026-02-05 00:10:35 +0100

    Hotfix p2

--- pokefusion/tools/dbutils.py
+++ pokefusion/tools/dbutils.py
@@ -1,27 +1,24 @@
-import json
 import logging
 import os
 
-from peewee import SqliteDatabase, TextField
+from peewee import SqliteDatabase
 from peewee_migrate import Router
-from playhouse.kv import KeyValue
 
-from configmanager import DatabaseConfig
+from pokefusion.configmanager import DatabaseConfig
 from pokefusion.models.models import BaseModel
+
+# from playhouse.kv import KeyValue
 
 logger = logging.getLogger(__name__)
 
 
-def create_migration(config: DatabaseConfig) -> None:
+def create_migration(config: DatabaseConfig, migration_name) -> None:
     migrate_dir = os.path.join("pokefusion", "migrations")
     router = Router(SqliteDatabase(config.path, pragmas=config.pragmas), migrate_dir=migrate_dir,
                     ignore=[BaseModel._meta.name], logger=logger)
 
     # Create migration
-    router.create("fix_default_free_rerolls", auto="pokefusion.models")
-
-    # Run migration/migrations
-    router.run("fix_default_free_rerolls")
+    router.create(migration_name, auto="pokefusion.models")
 
     # Run all unapplied migrations
     router.run()
@@ -34,11 +31,18 @@ def run_migrations(config: DatabaseConfig) -> None:
     router.run()
 
 
-def keyval_store(config: DatabaseConfig) -> None:
-    db = SqliteDatabase(config.path, pragmas=config.pragmas)
-    kv = KeyValue(database=db, ordered=True, table_name="keyval", value_field=TextField())
-    kv["channels"] = json.dumps([1, 2, 3, 4])
-    kv["reminder_time"] = json.dumps("10:38")
-    channels = json.loads(kv["channels"])
-    print(channels)
-    print(type(channels))
+def rollback_migration(config: DatabaseConfig) -> None:
+    migrate_dir = os.path.join("pokefusion", "migrations")
+    router = Router(SqliteDatabase(config.path, pragmas=config.pragmas), migrate_dir=migrate_dir,
+                    ignore=[BaseModel._meta.name], logger=logger)
+
+    router.rollback()
+
+# def keyval_store(config: DatabaseConfig) -> None:
+#     db = SqliteDatabase(config.path, pragmas=config.pragmas)
+#     kv = KeyValue(database=db, ordered=True, table_name="keyval", value_field=TextField())
+#     kv["channels"] = json.dumps([1, 2, 3, 4])
+#     kv["reminder_time"] = json.dumps("10:38")
+#     channels = json.loads(kv["channels"])
+#     print(channels)
+#     print(type(channels))
