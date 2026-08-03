f7b5c4020afb660d30aeebac895f72ea969b0fb5
jinai <7669687+jinai@users.noreply.github.com>
2026-08-03 02:39:41 +0200

    Remove db init from the CLI
    Use migrations apply instead

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import typer
 
-from pokefusion.cli.db import db_app
 from pokefusion.cli.dex import dex_app
 from pokefusion.cli.migrations import migrations_app
 from pokefusion.cli.run import run_bot
@@ -10,7 +9,6 @@ app = typer.Typer(no_args_is_help=True)
 
 app.command("run")(run_bot)
 app.add_typer(migrations_app, name="migrations")
-app.add_typer(db_app, name="db")
 app.add_typer(dex_app, name="dex")
 app.add_typer(tools_app, name="tools")
 
